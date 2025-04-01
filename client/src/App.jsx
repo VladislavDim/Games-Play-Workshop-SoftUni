c93142e31a3536b62d644d830ff5053c9bcd94b4
Dimitrov Vladislav <64010169+VladislavDim@users.noreply.github.com>
2025-04-01 10:33:22 +0300

    Extract Register component and add route for it

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router'
 import Header from './components/header/Header'
 import Home from './components/home/Home'
 import Login from './components/login/Login'
+import Register from './components/Register/Register'
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
                 <Routes>
                     <Route path='/' element={<Home />} />
                     <Route path='/login' element={<Login />} />
+                    <Route path='/register' element={<Register />} />
                 </Routes>
                 <Home />
             </main>
