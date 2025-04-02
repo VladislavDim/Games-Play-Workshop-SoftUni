459bccc6ab5b801e4ee312a7496597b143ea7915
Dimitrov Vladislav <64010169+VladislavDim@users.noreply.github.com>
2025-04-02 20:48:37 +0300

    Implemented game display functionality in the catalog.

--- client/src/api/gameService.js
+++ client/src/api/gameService.js
@@ -3,8 +3,9 @@ import request from "../utils/request.js";
 const baseUrl = 'http://localhost:3030/jsonstore/games';
 
 export default {
-    getAll() {
-        return request.get(baseUrl);
+    async getAll() {
+        const result = await request.get(baseUrl);
+        return Object.values(result);
     },
     create(gameData) {
         return request.post(baseUrl, gameData);
--- client/src/components/game-catalog/GameCatalog.jsx
+++ client/src/components/game-catalog/GameCatalog.jsx
@@ -1,31 +1,24 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import gameService from "../../api/gameService";
 import GameCatalogItem from "../game-catalog-item/GameCatalogItem.jsx";
 
 export default function GameCatalog() {
+    const [games, setGames] = useState([]);
+
     useEffect(() => {
         gameService.getAll()
-            .then(r => {
-                console.log(r);
-            });
+            .then(setGames);
     }, []);
-    
+
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
 
-            </div>
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="/images/avatar-1.jpg" />
-                    <h6>Action</h6>
-                    <h2>MineCraft</h2>
-                    <a href="#" className="details-button">Details</a>
-                </div>
-            </div>
+            {games.length > 0
+                ? games.map(game => <GameCatalogItem key={game._id} {...game} />)
+                : <h3 className="no-articles">No articles yet</h3>
+            }
 
-            {/* Display paragraph: If there is no games  */}
-            <h3 className="no-articles">No articles yet</h3>
-        </section>
+        </section >
     );
 }
