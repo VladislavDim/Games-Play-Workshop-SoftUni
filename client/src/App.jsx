9b044fdbac9b39af16e25ca19d51824236b7a38b
Dimitrov Vladislav <64010169+VladislavDim@users.noreply.github.com>
2025-04-05 08:01:19 +0300

    Add conditional navbar links based on auth state

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -42,7 +42,6 @@ function App() {
                         <Route path='/games/catalog' element={<GameCatalog />} />
                         <Route path='/logout' element={<Logout />} />
                     </Routes>
-                    <Home />
                 </main>
             </div>
         </UserContext.Provider>
--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,19 +1,26 @@
+import { useContext } from "react";
 import { Link } from "react-router";
+import { UserContext } from "../../context/userContext";
 
 export default function Header() {
+    const { email } = useContext(UserContext);
+
     return (
         <header>
             <h1><Link className="home" to="/">GamesPlay</Link></h1>
             <nav>
                 <Link to="/games/catalog">All games</Link>
-                <div id="user">
-                    <Link to="/games/create">Create Game</Link>
-                    <Link to="/logout">Logout</Link>
-                </div>
-                <div id="guest">
-                    <Link to="/login">Login</Link>
-                    <Link to="/register">Register</Link>
-                </div>
+                {email
+                    ? <div id="user">
+                        <Link to="/games/create">Create Game</Link>
+                        <Link to="/logout">Logout</Link>
+                        {email}
+                    </div>
+                    : <div id="guest">
+                        <Link to="/login">Login</Link>
+                        <Link to="/register">Register</Link>
+                    </div>
+                }
             </nav>
         </header>
     );
